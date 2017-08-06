cea2ea46efb5d89a1e68dd2ea54baf495a29d927
athammer <athammer@buffalo.edu>
2017-08-06 15:56:57 +0000

    update

--- main.go
+++ main.go
@@ -26,5 +26,5 @@ func main() {
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 
-	log.Fatal(http.ListenAndServe(":" + "8080", router))
+	log.Fatal(http.ListenAndServe(":" + port, router))
 }
